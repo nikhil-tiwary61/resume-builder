99d6c816a2198e3036f0740ab7d9a4835ba9eeec
Nikhil Tiwary <ntiwary61@gmail.com>
2023-06-04 20:45:52 +0530

    Add file structure for the app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  const name = "Tim";
+  return (
+    <>
+      <h1>Resume</h1>
+      <h2>{name}</h2>
+      <br />
+    </>
+  );
+}
+
+export default App;
